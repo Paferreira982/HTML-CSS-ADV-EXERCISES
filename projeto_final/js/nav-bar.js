b719031aaa8f32025ca2c5dde280d9d927f70172
Paferreira982 <paferreira982@hotmail.com>
2021-10-22 14:45:06 -0300

    adicao de variaveis de css e responsividade com imgs para celulares

--- projeto_final/js/nav-bar.js
+++ projeto_final/js/nav-bar.js
@@ -4,3 +4,11 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 });
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
